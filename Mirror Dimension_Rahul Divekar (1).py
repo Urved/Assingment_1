5d8a728e376d671d15e9770fd99ff8b4d7d7747f
Rahul Divekar <100591162+Urved@users.noreply.github.com>
2022-04-17 23:33:38 +0530

    Add files via upload

---
+++ Mirror Dimension_Rahul Divekar (1).py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Write a Python program that accepts a word from the user and reverse it.
+#Input : Edyoda
+#output: adoydE
+
+Name =input()
+print(Name[::-1])
+
